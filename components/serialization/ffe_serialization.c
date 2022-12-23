902de1d5c50bc04af502946d28ea1751be19f8ab
Liam_H <liam@mosaicmanufacturing.com>
2022-12-23 22:36:01 +0000

    serialization wip

--- components/serialization/ffe_serialization.c
+++ components/serialization/ffe_serialization.c
@@ -1,5 +1,4 @@
 #include "ffe_serialization.h"
 
-uint16_t ffe_serialization_locate_encoded_message(uint8_t raw_data, uint16_t raw_data_len) {
     
 }
--- components/serialization/ffe_serialization.h
+++ components/serialization/ffe_serialization.h
@@ -1,7 +1,24 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdbool.h>
+
+// 0..4       4                    5-19                         
+// <checksum>|<from><ackee count>|(<ackee address><sequence>)*|(<zero bits><to>|<message>)?
+// 4 bytes    4bits  4bits          4 bits         4 bits        4 bits   4 bits  bytes*
+
+// longest possible decoded message = 277
+// longest possible encoded message = 370
+// longest possible encoded message with start and end markers = 372
+
+struct ffe_serialization_Packet {
+    uint8_t from;
+    uint16_t ackee_bitmask;
+    uint8_t ackee_sequences[16];
+    bool has_message;
+    uint8_t to;
+    
+};
 
 // no newline plz!!!
-// return value of 0 means no message found
-uint16_t ffe_serialization_locate_encoded_message(uint8_t raw_data, uint16_t raw_data_len);
+bool ffe_serialization_deserialize(struct ffe_serialization_Packet *dest, uint8_t *message_dest, uint8_t *raw_input, uint16_t raw_input_len);
